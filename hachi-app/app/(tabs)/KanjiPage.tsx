1d465361378073ce85d249f893e9701dd642f90e
SuperPupPresent <antfleck07@gmail.com>
2025-05-21 15:08:59 -0400

    Added a third page

---
+++ hachi-app/app/(tabs)/KanjiPage.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from '@/components/Themed';
+import ImageBG from '@/components/imageBG';
+import KanjiList from '@/components/KanjiList';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Kanji</Text>
+      <KanjiList /> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
