d4390465116077266048b49c99da7a091ac15814
SuperPupPresent <antfleck07@gmail.com>
2025-06-04 19:48:03 -0400

    Fixed me accidentally adding another whole project

--- hachi-app/app/KanjiInfo.tsx
+++ hachi-app/app/KanjiInfo.tsx
@@ -1,7 +1,31 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet } from "react-native";
+import { Text, View } from "@/components/Themed";
+import SQLite from "react-native-sqlite-storage";
+
+// const db = SQLite.openDatabase(
+//   {
+//     name: "kanji_entries.db",
+//     location: "default",
+//   },
+//   () => {},
+//   (error) => {
+//     console.error(error);
+//     throw Error("Could not connect to database");
+//   }
+// );
+
+// const createTable = () => {
+//   db.transaction((tx) => {
+//     tx.executeSql(
+//       "CREATE TABLE IF NOT EXISTS " +
+//         "Kanji " +
+//         "(id INTEGER PRIMARY KEY AUTOINCREMENT, Character TEXT NOT NULL, on_readings TEXT, kun_readings TEXT, meanings TEXT, jlpt_level INTEGER, examples TEXT, update_at TEXT DEFAULT CURRENT_TIMESTAMP)"
+//     );
+//   });
+// };
 
 export default function KanjiInfoScreen() {
+  //createTable();
   return (
     <View>
       <Text style={styles.title}>Kanji</Text>
@@ -12,17 +36,17 @@ export default function KanjiInfoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#2D3A3A',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#2D3A3A",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
